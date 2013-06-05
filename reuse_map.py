5ecff0b396d766f6139a66e7580f66390dc02ad7
Jess Kenney <seveneightn9ne@gmail.com>
2013-06-04 21:58:19 -0400

    added regex searches

--- reuse_map.py
+++ reuse_map.py
@@ -1,4 +1,37 @@
 
 import re
 
-claimed = [r"claimed", r"]
+claimed = [
+	"(.*) claimed",
+	"claimed (.+)", 
+	"(.*) taken", 
+	"(.*) gone", 
+	"took (.+)"
+]
+not_claimed = [
+	"(.+) not claimed", 
+	"(.+) still there", 
+	"(.+) remain", 
+	"(.+) remains", 
+	"(.+) not taken"
+]
+quantity = [
+	"both", 
+	"all", 
+	"one", 
+	"a few"
+]
+bldg = "([new]*\d\d?[newabcp])"
+room = bldg+"-[a-z]?\d\d\d?\d?"
+places = [
+	"("+bldg+" loading dock)", 
+	"(lobby \d+)", 
+	"(room "+room+")", 
+	"(outside (?:of)? "+room+")", 
+	"(\w+ floor (?:of)? b(?:ui)?ld(?:in)?g? "+bldg+")",
+	"(b(?:ui)?ld(?:in)?g? "+bldg+",? floor \w+)",
+	"(b(?:ui)?ld(?:in?)g? "+bldg+",? \w+ floor)",
+	"(.+ floor .+ .+)",
+	"("+room+")", 
+	"(b(?:ui)?ld(?:in)?g? "+bldg+")"
+]
