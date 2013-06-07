6b1618aa7ffcf07582fa6259f42b2f352ebd1f5b
Miles Steele <miles@milessteele.com>
2013-06-06 21:21:21 -0400

    move non-django files to scripts/

--- scripts/gmail_doodle.py
+++ scripts/gmail_doodle.py
@@ -1,14 +1,15 @@
+from getpass import getpass
 from imapclient import IMAPClient
 
-HOST = 'imap.mail.yahoo.com'
-USERNAME = 'reusemap'
-PASSWORD = 'xert-ra-wha-de'
+HOST = 'imap.gmail.com'
+USERNAME = 'mlsteele00@gmail.com'
+PASSWORD = getpass()
 ssl = True
 
 server = IMAPClient(HOST, use_uid=True, ssl=ssl)
 server.login(USERNAME, PASSWORD)
 
-select_info = server.select_folder('INBOX')
+select_info = server.select_folder('reuse')
 print('%d messages in INBOX' % select_info['EXISTS'])
 
 messages = server.search(['NOT DELETED'])
@@ -16,8 +17,8 @@ print("%d messages that aren't deleted" % len(messages))
 
 print()
 print("Messages:")
-response = server.fetch(messages, ['FLAGS', 'RFC822'])
+response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
 for msgid, data in response.iteritems():
     print('   ID %d: %s bytes, flags=%s' % (msgid,
-                                            data['RFC822'],
+                                            data['RFC822.SIZE'],
                                             data['FLAGS']))
