f4a28d38459b1ec355aea36cefa816bfbe156f06
Jessica Kenney <seveneightn9ne@gmail.com>
2013-06-08 02:04:40 -0400

    updated parser

--- reuse/email_parser.py
+++ reuse/email_parser.py
@@ -1,26 +1,67 @@
 import sys
 import re
+import itertools
 
 from emails.models import EmailMessage, EmailHeader
 
+claimed = [
+    "(.*)claimed",
+    "claimed (.+)", 
+    "(.*)taken", 
+    "(.*)gone", 
+    "took (.+)"
+]
+not_claimed = [
+    "(.+) not claimed", 
+    "(.+) still there", 
+    "(.+) remain", 
+    "(.+) remains", 
+    "(.+) not taken"
+]
+quantity = [
+    "both", 
+    "all", 
+    "one", 
+    "a few"
+]
+bldg = "[^\w]([new]*\d\d?[newabcp]?)"
+room = bldg+"-[a-z]?\d\d\d?\d?"
+places = [
+    "("+bldg+" loading dock)", 
+    "(lobby \d+)", 
+    "(room "+room+")", 
+    "(outside (?:of)? "+room+")", 
+    "(\w+ floor (?:of)? b(?:ui)?ld(?:in)?g? "+bldg+")",
+    "(b(?:ui)?ld(?:in)?g? "+bldg+",? floor \w+)",
+    "(b(?:ui)?ld(?:in?)g? "+bldg+",? \w+ floor)",
+    "(.+ floor .+ .+)",
+    "("+room+")", 
+    "(b(?:ui)?ld(?:in)?g? "+bldg+")",
+    "("+bldg+"-\d\w\w floor)"
+]
+
 
 def test():
-    parse_single_item_message(get_single_item_messages()[0])
+    parse_single_item_messages(get_messages())
 
 
-def get_single_item_messages():
-    return [
-        EmailMessage.objects.get(uid=33, username='reusemap', host='imap.mail.yahoo.com'),
-        EmailMessage.objects.get(uid=39, username='reusemap', host='imap.mail.yahoo.com'),
-        EmailMessage.objects.get(uid=40, username='reusemap', host='imap.mail.yahoo.com'),
-    ]
+def get_messages():
+    # q = []
+    # for suid in range(1,40):
+    #     q.append(EmailMessage.objects.get(uid=suid, username='reusemap', host='imap.mail.yahoo.com'))
+    return EmailMessage.objects.all()
+    # return [
+    #     EmailMessage.objects.get(uid=33, username='reusemap', host='imap.mail.yahoo.com'),
+    #     EmailMessage.objects.get(uid=39, username='reusemap', host='imap.mail.yahoo.com'),
+    #     EmailMessage.objects.get(uid=40, username='reusemap', host='imap.mail.yahoo.com'),
+    # ]
 
 
 def extract_text(emailmessage):
     msg = emailmessage.get_email()
     plain_parts = filter(lambda part: part.get_content_type() == 'text/plain', msg.walk())
     full_text = reduce(lambda memo, part: memo + part.get_payload(), plain_parts, '')
-    return full_text.replace('\r\n', '\n')
+    return full_text.replace('\r\n', '\n').replace("To sub/unsubscribe or to see the list rules:  http://mailman.mit.edu/mailman/listinfo/reuse","").replace("\n\n","\n")
 
 
 def print_with_invisibles(s):
@@ -32,13 +73,29 @@ def print_with_invisibles(s):
         else:
             sys.stdout.write(char)
 
+def print_dict(dictionary):
+    for key,val in dictionary.iteritems():
+        print "["+str(key)+"]:"
+        print "\t"+str(val)+"\n"
+def parse_single_item_messages(messages):
+    locations = {}
+    for message in messages:
+        #print 'parsing '+message
+        location, full_text = parse_single_item_message(message)
+        locations[full_text] = location
+    print_dict(locations)
+        
 def parse_single_item_message(emailmessage):
     full_text = extract_text(emailmessage)
 
-    print_with_invisibles(full_text)
-
+    #print_with_invisibles(full_text)
+    #print(full_text.lower())
     print '\n' + '-' * 20 + '\n'
 
-    location = re.search(r"E(.*)-(.*)th", full_text).groups()
-
-    print 'location: ', location
+    for place in places:
+        #print 'searching for '+place
+        location_search = re.search(r""+place, full_text.lower())
+        if location_search:
+            return location_search.groups(), full_text
+        else:
+            return None, None
